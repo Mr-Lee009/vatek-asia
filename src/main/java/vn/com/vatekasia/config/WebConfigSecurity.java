75643859bc992a03317688e62ec95f547bea7ce5
mr.lee <anhduc2781997@gmail.com>
2022-12-02 15:15:25 +0700

    update code

--- src/main/java/vn/com/vatekasia/config/WebConfigSecurity.java
+++ src/main/java/vn/com/vatekasia/config/WebConfigSecurity.java
@@ -1,11 +1,12 @@
 package vn.com.vatekasia.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -34,7 +35,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(org.springframework.security.config.annotation.web.builders.WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(
                 "/css/**",
                 "/js/**",
